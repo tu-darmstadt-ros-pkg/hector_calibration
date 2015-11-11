315f38ce02100f00b80533322379c8625ce02d40
Martin Oehler <martin.sven.oehler@gmail.com>
2015-11-11 22:35:07 +0100

    implemented simple point cloud aggregation

--- calibration_test/calibration_test/src/example.cpp
+++ calibration_test/calibration_test/src/example.cpp
@@ -3,33 +3,38 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
-#include <pcl_ros/filters/voxel_grid.h>
 
 ros::Publisher pub;
+pcl::PointCloud<pcl::PointXYZI> sum_cloud;
+unsigned int counter = 0;
+bool init = false;
 
-void 
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-  // Container for original & filtered data
-  pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
-  pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-  pcl::PCLPointCloud2 cloud_filtered;
+  pcl::PointCloud<pcl::PointXYZI> cloud;
 
-  // Convert to PCL data type
-  pcl_conversions::toPCL(*cloud_msg, *cloud);
+  if (init) {
+    pcl::fromROSMsg(*cloud_msg, sum_cloud);
+  } else {
+    // Convert to PCL data type
+    pcl::fromROSMsg(*cloud_msg, cloud);
+    sum_cloud.header.frame_id = cloud.header.frame_id;
+    sum_cloud += cloud;
+  }
 
-  // Perform the actual filtering
-  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-  sor.setInputCloud (cloudPtr);
-  sor.setLeafSize (0.1, 0.1, 0.1);
-  sor.filter (cloud_filtered);
 
-  // Convert to ROS data type
-  sensor_msgs::PointCloud2 output;
-  pcl_conversions::fromPCL(cloud_filtered, output);
+  if (counter == 200) {
+    // Convert to ROS data type
+    sensor_msgs::PointCloud2 output;
+    pcl::toROSMsg(sum_cloud, output);
 
-  // Publish the data
-  pub.publish (output);
+    // Publish the data
+    pub.publish (output);
+
+    counter = 0;
+  } else {
+    counter++;
+  }
 }
 
 int
@@ -40,10 +45,10 @@ main (int argc, char** argv)
   ros::NodeHandle nh;
 
   // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe("input", 1, cloud_cb);
+  ros::Subscriber sub = nh.subscribe("/scan_cloud_filtered", 1, cloud_cb);
 
   // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
+  pub = nh.advertise<sensor_msgs::PointCloud2>("scan_cloud_accumulated", 1);
 
   // Spin
   ros::spin ();
