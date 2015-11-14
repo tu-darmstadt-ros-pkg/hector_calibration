0515795a0174082819b078219dcf8f3088c56136
Martin Oehler <martin.sven.oehler@gmail.com>
2015-11-14 20:16:11 +0100

    implementation

---
+++ lidar_calibration/src/capture_data_node.cpp
@@ -0,0 +1,7 @@
+#include <lidar_calibration/point_cloud_aggregator.h>
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "capture_data_node");
+
+	ros::NodeHandle nh;
+}
---
+++ lidar_calibration/src/point_cloud_aggregator.cpp
@@ -0,0 +1,4 @@
+#include <lidar_calibration/point_cloud_aggregator.h>
+
+namespace hector_calibration {
+}
