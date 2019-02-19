6e49d75669c421442bf724503f39565a044eb165
Martin Oehler <mail@martinoehler.de>
2019-02-19 17:38:48 +0100

    Removed unnecessary glog init call

--- lidar_calibration/src/lidar_calibration_node.cpp
+++ lidar_calibration/src/lidar_calibration_node.cpp
@@ -5,7 +5,6 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "lidar_calibration_node");
 
   ros::NodeHandle nh;
-  google::InitGoogleLogging(argv[0]);
   hector_calibration::lidar_calibration::LidarCalibration calibration(nh);
 
   boost::program_options::options_description desc("Allowed options");
